969e02d70065e02c51ca7ae4ccf065e585130385
loligator <loligator704@gmail.com>
2016-12-09 20:51:58 -0500

    Debugs out!

--- javascripts/gallery.js
+++ javascripts/gallery.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 	$(".subImage").click(function() {
-		alert("Clicked!");
 		var source = $(this).attr('src');
-		alert(source);
 		$("#mainGallery").fadeOut(500, function(){
 			$(this).attr("src", source);
 			$(this).load(function(){
