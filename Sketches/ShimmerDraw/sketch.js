65097173da5726ead508ee316b7149484dacf550
loligator <loligator704@gmail.com>
2018-02-14 12:21:56 -0500

    ShimmerDraw!

---
+++ Sketches/ShimmerDraw/sketch.js
@@ -0,0 +1,40 @@
+let boxes = [];
+
+function setup() {
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+	background(220);
+	push();
+	fill(0);
+	textSize(32);
+	textFont('Helvetica');
+	text('Draw!', 10, 30);
+	pop();
+	if(mouseIsPressed){
+		boxes.push(new Box(mouseX, mouseY, random(10, 20)));
+	}
+  
+	for(let box of boxes){
+		box.show();
+	}
+}
+
+class Box{
+	constructor(x, y, s){
+		this.x = x;
+		this.y = y;
+		this.size = s;
+	}
+	
+	show(){
+		push();
+		noStroke();
+		let distToMouse = dist(mouseX, mouseY, this.x, this.y);
+		fill(255-random(0,255)-distToMouse,255-random(0,255) - distToMouse,255-random(0,255) - distToMouse);
+		ellipse(this.x, this.y, this.size, this.size);
+		pop();
+	}
+}
